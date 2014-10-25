5d1fd880a39b3642c3339a229cca272278c1aac3
Megan Chen <mcgauss1181@gmail.com>
2014-10-25 13:10:52 -0400

    Fix about page url

--- crashcourse/app/urls.py
+++ crashcourse/app/urls.py
@@ -3,4 +3,5 @@ from app import views
 
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
+        url(r'^about/$', views.about, name='about'),
         url(r'^add_task/$', views.add_task, name='add_task'))
