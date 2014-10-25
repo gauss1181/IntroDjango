7c1e0aca5883521f7dec394201ad9257ca8776f2
Megan Chen <mcgauss1181@gmail.com>
2014-10-25 00:39:26 -0400

    Add categories to index view

--- crashcourse/app/views.py
+++ crashcourse/app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from app.models import Category
 
 def index(request):
-    return render(request, 'app/index.html')
+    category_list = Category.objects.all()
+    return render(request, 'app/index.html', {'categories': category_list})
